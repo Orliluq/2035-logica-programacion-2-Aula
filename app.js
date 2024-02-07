454c3153834c5ae365303f90e7c888c11d8f4cb4
Orli Dun <122529721+Orliluq@users.noreply.github.com>
2024-02-06 23:03:37 -0400

    Curso 2 Alura Latam

---
+++ app.js
@@ -0,0 +1,456 @@
+/* let numeroSecreto = generarNumeroSecreto();
+let intentos = 0;
+
+console.log(numeroSecreto);
+
+function asignarTextoElemento(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
+}
+
+function verificarIntento() {
+    let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
+    
+    if (numeroDeUsuario === numeroSecreto) {
+        console.log('Acertaste el número!');
+    } 
+    return;
+}
+
+
+function generarNumeroSecreto() {
+    return Math.floor(Math.random()*10)+1;
+
+}
+
+asignarTextoElemento('h1','Juego del número secreto!');
+asignarTextoElemento('p',`Indica un número del 1 al 10`); */
+
+/* let numeroSecreto = generarNumeroSecreto();
+let intentos = 0;
+
+
+function asignarTextoElemento(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
+}
+
+function verificarIntento() {
+    let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
+    
+    if (numeroDeUsuario === numeroSecreto) {
+        asignarTextoElemento('p',`Acertaste el número en ${intentos} ${(intentos === 1) ? 'vez' : 'veces'}`);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    } else {
+        //El usuario no acertó.
+        if (numeroDeUsuario > numeroSecreto) {
+            asignarTextoElemento('p','El número secreto es menor');
+        } else {
+            asignarTextoElemento('p','El número secreto es mayor');
+        }
+        intentos++;
+        limpiarCaja();
+    }
+    return;
+}
+
+function limpiarCaja() {
+    document.querySelector('#valorUsuario').value = '';
+}
+
+function generarNumeroSecreto() {
+    return Math.floor(Math.random()*10)+1;
+
+}
+
+function condicionesIniciales() {
+    asignarTextoElemento('h1','Juego del número secreto!');
+    asignarTextoElemento('p',`Indica un número del 1 al 10`);
+    numeroSecreto = generarNumeroSecreto();
+    intentos = 1;
+    console.log(numeroSecreto);
+}
+
+function reiniciarJuego() {
+    //limpiar caja
+    limpiarCaja();
+    //Indicar mensaje de intervalo de números 
+    //Generar el número aleatorio
+    //Inicializar el número intentos
+    condicionesIniciales();
+    //Deshabilitar el botón de nuevo juego
+    document.querySelector('#reiniciar').setAttribute('disabled','true');
+    
+}
+
+condicionesIniciales(); */
+
+/* let numeroSecreto = 0;
+let intentos = 0;
+let listaNumerosSorteados = [];
+let numeroMaximo = 10;
+
+
+
+function asignarTextoElemento(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
+}
+
+function verificarIntento() {
+    let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
+    
+    if (numeroDeUsuario === numeroSecreto) {
+        asignarTextoElemento('p',`Acertaste el número en ${intentos} ${(intentos === 1) ? 'vez' : 'veces'}`);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    } else {
+        //El usuario no acertó.
+        if (numeroDeUsuario > numeroSecreto) {
+            asignarTextoElemento('p','El número secreto es menor');
+        } else {
+            asignarTextoElemento('p','El número secreto es mayor');
+        }
+        intentos++;
+        limpiarCaja();
+    }
+    return;
+}
+
+function limpiarCaja() {
+    document.querySelector('#valorUsuario').value = '';
+}
+
+function generarNumeroSecreto() {
+    let numeroGenerado =  Math.floor(Math.random()*numeroMaximo)+1;
+
+    console.log(numeroGenerado);
+    console.log(listaNumerosSorteados);
+    //Si ya sorteamos todos los números
+    if (listaNumerosSorteados.length == numeroMaximo) {
+        asignarTextoElemento('p','Ya se sortearon todos los números posibles');
+    } else {
+        //Si el numero generado está incluido en la lista 
+        if (listaNumerosSorteados.includes(numeroGenerado)) {
+            return generarNumeroSecreto();
+        } else {
+            listaNumerosSorteados.push(numeroGenerado);
+            return numeroGenerado;
+        }
+    }
+}
+
+function condicionesIniciales() {
+    asignarTextoElemento('h1','Juego del número secreto!');
+    asignarTextoElemento('p',`Indica un número del 1 al ${numeroMaximo}`);
+    numeroSecreto = generarNumeroSecreto();
+    intentos = 1;
+    console.log(numeroSecreto);
+}
+
+function reiniciarJuego() {
+    //limpiar caja
+    limpiarCaja();
+    //Indicar mensaje de intervalo de números 
+    //Generar el número aleatorio
+    //Inicializar el número intentos
+    condicionesIniciales();
+    //Deshabilitar el botón de nuevo juego
+    document.querySelector('#reiniciar').setAttribute('disabled','true');
+    
+}
+
+condicionesIniciales(); */
+
+let numeroSecreto = 0;
+let intentos = 0;
+let listaNumerosSorteados = [];
+let numeroMaximo = 10;
+
+
+
+function asignarTextoElemento(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
+}
+
+function verificarIntento() {
+    let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
+    
+    if (numeroDeUsuario === numeroSecreto) {
+        asignarTextoElemento('p',`Acertaste el número en ${intentos} ${(intentos === 1) ? 'vez' : 'veces'}`);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    } else {
+        //El usuario no acertó.
+        if (numeroDeUsuario > numeroSecreto) {
+            asignarTextoElemento('p','El número secreto es menor');
+        } else {
+            asignarTextoElemento('p','El número secreto es mayor');
+        }
+        intentos++;
+        limpiarCaja();
+    }
+    return;
+}
+
+function limpiarCaja() {
+    document.querySelector('#valorUsuario').value = '';
+}
+
+function generarNumeroSecreto() {
+    let numeroGenerado =  Math.floor(Math.random()*numeroMaximo)+1;
+
+    console.log(numeroGenerado);
+    console.log(listaNumerosSorteados);
+    //Si ya sorteamos todos los números
+    if (listaNumerosSorteados.length == numeroMaximo) {
+        asignarTextoElemento('p','Ya se sortearon todos los números posibles');
+    } else {
+        //Si el numero generado está incluido en la lista 
+        if (listaNumerosSorteados.includes(numeroGenerado)) {
+            return generarNumeroSecreto();
+        } else {
+            listaNumerosSorteados.push(numeroGenerado);
+            return numeroGenerado;
+        }
+    }
+}
+
+function condicionesIniciales() {
+    asignarTextoElemento('h1','Juego del número secreto!');
+    asignarTextoElemento('p',`Indica un número del 1 al ${numeroMaximo}`);
+    numeroSecreto = generarNumeroSecreto();
+    intentos = 1;
+    console.log(numeroSecreto);
+}
+
+function reiniciarJuego() {
+    //limpiar caja
+    limpiarCaja();
+    //Indicar mensaje de intervalo de números 
+    //Generar el número aleatorio
+    //Inicializar el número intentos
+    condicionesIniciales();
+    //Deshabilitar el botón de nuevo juego
+    document.querySelector('#reiniciar').setAttribute('disabled','true');
+    
+}
+
+condicionesIniciales();
+
+
+
+/* let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Hora del Desafío';
+
+<button onclick="mostarMensajeEnLaConsola()" class="button">Console</button>
+
+function mostarMensajeEnLaConsola() {
+    console.log('El botón fue clicado!')
+}
+
+<button onclick="mostrarAlerta()" class="button">Alert</button>
+
+function mostrarAlerta() {
+    let ciudad = prompt("Por favor, ingresa el nombre de una ciudad de Brasil:");
+    alert("Estuve en " + ciudad + " y me acordé de ti.");
+}
+
+<button onclick="mostarAlerta()" class="button">Alert</button>
+
+function mostarAlerta(){
+    alert("AMO JS");
+} 
+
+ <button onclick="sumaDosNumeros()" class="button">Suma</button>
+
+function sumaDosNumeros(){
+    let primerNumero = parseInt(prompt('Digite el primer numero'));
+    let segunNumero = parseInt(prompt('Digite el segundo numero'));
+    let resultado = primerNumero + segunNumero;
+    alert('${primerNumero} + ${segunNumero} = ${resultado}')
+}
+
+
+function mostrarHola() {
+  console.log("¡Hola, mundo!");
+}
+
+mostrarHola();
+
+function mostrarHolaNombre(nombre) {
+  console.log(`¡Hola, ${nombre}!`);
+}
+
+mostrarHolaNombre("Alice");
+
+function calcularDoble(numero) {
+  return numero * 2;
+}
+
+let resultadoDoble = calcularDoble(5);
+console.log(resultadoDoble);
+
+function calcularPromedio(a, b, c) {
+  return (a + b + c) / 3;
+}
+
+let promedio = calcularPromedio(4, 7, 10);
+console.log(promedio);
+
+function encontrarMayor(a, b) {
+  return a > b ? a : b;
+}
+
+let numeroMayor = encontrarMayor(15, 9);
+console.log(numeroMayor);
+
+function cuadrado(numero) {
+  return numero * numero;
+}
+
+let resultado = cuadrado(2);
+console.log(resultado);
+
+
+function calcularIMC(altura, peso) {
+  var imc = peso / (altura * altura);
+  return imc;
+}
+
+function calcularFactorial(numero) {
+  if (numero === 0 || numero === 1) {
+    return 1;
+  } else {
+    return numero * calcularFactorial(numero - 1);
+  }
+} */
+// Ejemplo
+/* let numero = 5;
+let resultado = calcularFactorial(numero);
+console.log(`El factorial de ${numero} es ${resultado}`);
+
+
+function convertirDolaresAReales(dolares) {
+  var cotizacionDolar = 4.80;
+  var reales = dolares * cotizacionDolar;
+  return reales;
+} */
+// Ejemplo
+/* let valorEnDolar = 50;
+let valorEnReales = convertirDolaresAReales(valorEnDolar);
+console.log(`${valorEnDolar} dólares  es R$ ${valorEnReales}`);
+
+
+function calcularAreaYPerimetroRectangular(altura, anchura) {
+  var area = altura * anchura;
+  var perimetro = 2 * (altura + anchura);
+  console.log("Área: " + area);
+  console.log("Perímetro: " + perimetro);
+} */
+// Ejemplo 
+/* let altura = 3; // en metros
+let anchura = 5; // en metros
+calcularAreaYPerimetroRectangular(altura, anchura);
+
+
+function calcularAreaYPerimetroCircular(radio) {
+  var pi = 3.14;
+  var area = pi * radio * radio;
+  var perimetro = 2 * pi * radio;
+  console.log("Área: " + area);
+  console.log("Perímetro: " + perimetro);
+} */
+// Ejemplo 
+/* let radio = 4; // en metros
+calcularAreaYPerimetroCircular(radio);
+
+
+function mostrarTablaDeMultiplicar(numero) {
+  for (var i = 1; i <= 10; i++) {
+    var resultado = numero * i;
+    console.log(numero + " x " + i + " = " + resultado);
+  }
+} */
+// Ejemplo 
+/* let numero = 7;
+mostrarTablaDeMultiplicar(numero);
+
+let listaGenerica = [];
+
+let lenguagesDeProgramacion = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python'];
+
+lenguagesDeProgramacion.push('Java', 'Ruby', 'GoLang');
+
+function mostrarLenguagesSeparadamente() {
+  for (let i = 0; i < lenguagesDeProgramacion.length; i++) {
+    console.log(lenguagesDeProgramacion[i]);
+  }
+}
+
+mostrarLenguagesSeparadamente();
+
+function mostrarLenguagesReversoSeparadamente() {
+  for (let i = lenguagesDeProgramacion.length - 1; i >= 0; i--) {
+    console.log(lenguagesDeProgramacion[i]);
+  }
+}
+
+mostrarLenguagesReversoSeparadamente();
+
+function calcularMedia(lista) {
+  let suma = 0;
+  for (let i = 0; i < lista.length; i++) {
+    suma += lista[i];
+  }
+  return suma / lista.length;
+}
+
+let numeros = [10, 20, 30, 40, 50];
+let media = calcularMedia(numeros);
+console.log('Média:', media);
+
+function encontrarMayorMenor(lista) {
+  let mayor = lista[0];
+  let menor = lista[0];
+
+  for (let i = 1; i < lista.length; i++) {
+    if (lista[i] > mayor) {
+      mayor = lista[i];
+    }
+    if (lista[i] < menor) {
+      menor = lista[i];
+    }
+  }
+
+  console.log('Mayor:', mayor);
+  console.log('Menor:', menor);
+}
+
+let numeros = [15, 8, 25, 5, 12];
+encontrarMayorMenor(numeros);
+
+function calcularSuma(lista) {
+  let suma = 0;
+  for (let i = 0; i < lista.length; i++) {
+    suma += lista[i];
+  }
+  return suma;
+}
+ */
+/* let numeros = [15, 8, 25, 5, 12];
+let suma = calcularSuma(numeros);
+console.log('Suma:', suma);
+
+function encontrarIndiceElemento(lista, elemento) {
+  for (let i = 0; i < lista.length; i++) {
+    if (lista[i] === elemento) {
+      return i; // Retorna el índice del elemento encontrado
+    }
+  }
+  return -1; // Retorna -1 si el elemento no se encuentra en la lista
+}
+ */
